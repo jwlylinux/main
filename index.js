e1ad2bf7b0a5633d449f45f3fb101730a54c7c2d
root <root@zsc>
2024-09-01 19:38:54 +0800

    update

--- index.js
+++ index.js
@@ -1,61 +1,67 @@
-const path = require("path");
 const express = require("express");
+const axios = require("axios");
 const cors = require("cors");
-const morgan = require("morgan");
-const { init: initDB, Counter } = require("./db");
-
-const logger = morgan("tiny");
+const path = require("path");
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
-app.use(logger);
 
-// 首页
-app.get("/", async (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
+// 百度文心千帆 API 配置
+const apiKey = 'j0o1cQEo3gBbDAmXvJ7x61sE';
+const secretKey = 'tWxvmiLWCwTrlJ0Chq8OxymKUq52etHS';
+const appId = '101171466';
 
-// 更新计数
-app.post("/api/count", async (req, res) => {
-  const { action } = req.body;
-  if (action === "inc") {
-    await Counter.create();
-  } else if (action === "clear") {
-    await Counter.destroy({
-      truncate: true,
+// 获取 Access Token
+async function getAccessToken() {
+  const response = await axios.post('https://aip.baidubce.com/oauth/2.0/token', null, {
+    params: {
+      grant_type: 'client_credentials',
+      client_id: apiKey,
+      client_secret: secretKey,
+    },
+  });
+
+  return response.data.access_token;
+}
+
+// 调用百度文心千帆 API
+app.post("/api/ask", async (req, res) => {
+  const { question, role } = req.body;
+
+  try {
+    const accessToken = await getAccessToken();
+    
+    const response = await axios.post(`https://aip.baidubce.com/rpc/2.0/ai_custom_model/v1/${appId}`, {
+      question: question,
+      role: role,
+    }, {
+      headers: {
+        'Authorization': `Bearer ${accessToken}`
+      }
+    });
+
+    res.send({
+      code: 0,
+      data: response.data,
+    });
+  } catch (error) {
+    res.status(500).send({
+      code: 1,
+      message: error.toString(),
     });
   }
-  res.send({
-    code: 0,
-    data: await Counter.count(),
-  });
 });
 
-// 获取计数
-app.get("/api/count", async (req, res) => {
-  const result = await Counter.count();
-  res.send({
-    code: 0,
-    data: result,
-  });
-});
-
-// 小程序调用，获取微信 Open ID
-app.get("/api/wx_openid", async (req, res) => {
-  if (req.headers["x-wx-source"]) {
-    res.send(req.headers["x-wx-openid"]);
-  }
+// 首页 - 提供简单的表单页面
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 const port = process.env.PORT || 80;
 
-async function bootstrap() {
-  await initDB();
-  app.listen(port, () => {
-    console.log("启动成功", port);
-  });
-}
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
-bootstrap();
